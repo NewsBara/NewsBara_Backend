fdf784c03b2c7b2c349ec545c81ef58b055e392a
bigtr3 <tmddms1002@gmail.com>
2025-06-17 20:51:23 +0900

    포인트 획득 dto 생성

--- src/main/java/com/example/newsbara/user/dto/req/UserSignupReqDto.java
+++ src/main/java/com/example/newsbara/user/dto/req/UserSignupReqDto.java
@@ -1,11 +1,15 @@
 package com.example.newsbara.user.dto.req;
 
 import com.example.newsbara.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor  // 이 어노테이션 추가!
+@Builder
 public class UserSignupReqDto {
     private String email;
     private String password;
@@ -21,4 +25,3 @@ public class UserSignupReqDto {
                 .build();
     }
 }
-

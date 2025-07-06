dbbe4b80ad0081e003de457714c41d238bd3b17e
bigtr3 <tmddms1002@gmail.com>
2025-07-06 15:29:53 +0900

    feat : 친구 요청 목록 API 서비스 로직 구현

--- src/main/java/com/example/newsbara/user/repository/FollowRepository.java
+++ src/main/java/com/example/newsbara/user/repository/FollowRepository.java
@@ -2,12 +2,20 @@ package com.example.newsbara.user.repository;
 
 import com.example.newsbara.user.domain.Follow;
 import com.example.newsbara.user.domain.User;
+import com.example.newsbara.user.domain.enums.FollowStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     Optional<Follow> findByFollowerAndFollowing(User follower, User following);
+
+    // 받은 친구 요청 목록 (최신순)
+    @Query("SELECT f FROM Follow f WHERE f.following = :user AND f.status = :status ORDER BY f.createdAt DESC")
+    List<Follow> findPendingRequestsByUser(@Param("user") User user, @Param("status") FollowStatus status);
 }
--- src/main/java/com/example/newsbara/user/service/FollowService.java
+++ src/main/java/com/example/newsbara/user/service/FollowService.java
@@ -2,11 +2,13 @@ package com.example.newsbara.user.service;
 
 import com.example.newsbara.global.common.apiPayload.code.status.ErrorStatus;
 import com.example.newsbara.global.common.apiPayload.exception.GeneralException;
+import com.example.newsbara.history.dto.res.HistoryResDto;
 import com.example.newsbara.user.domain.Follow;
 import com.example.newsbara.user.domain.User;
 import com.example.newsbara.user.domain.enums.FollowStatus;
 import com.example.newsbara.user.dto.req.FollowAddReqDto;
 import com.example.newsbara.user.dto.res.FollowAddResDto;
+import com.example.newsbara.user.dto.res.FollowResListDto;
 import com.example.newsbara.user.repository.FollowRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +17,9 @@ import com.example.newsbara.user.repository.UserRepository;
 
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -62,5 +66,14 @@ public class FollowService {
         return FollowAddResDto.fromEntity(followRepository.save(follow));
     }
 
+    @Transactional(readOnly = true)
+    public List<FollowResListDto> getRequests(Principal principal) {
+        User user = userRepository.findByEmail(principal.getName())
+                .orElseThrow(() -> new GeneralException(ErrorStatus.USER_NOT_FOUND));
 
+
+        List<Follow> pendingRequests = followRepository.findPendingRequestsByUser(user, FollowStatus.PENDING);
+
+        return pendingRequests.stream().map(FollowResListDto::fromEntity).collect(Collectors.toList());
+    }
 }

2d1533b4e984e5a2740e9c469a75a8911fba420d
bigtr3 <tmddms1002@gmail.com>
2025-07-24 18:02:13 +0900

    스크립트 API 컨트롤러 설정

---
+++ src/main/java/com/example/newsbara/ai/service/ExternalApiService.java
@@ -0,0 +1,12 @@
+package com.example.newsbara.ai.service;
+
+import com.example.newsbara.ai.dto.req.AnalysisReqDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExternalApiService {
+    public List<AnalysisReqDto> analyzeVideo(String videoId) {
+    }
+}
